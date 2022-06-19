d74ac634ba18787bdeee1971f7b38bd8e798042d
Daryl Rogony <darylrogony@gmail.com>
2022-06-20 00:37:19 +0300

    commit tree 40a70ed68d4a81da1aa9e0372b1655445b9c9e66

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  def initialize(url)
+    @url = url
+  end
+
+#  URL = " https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+ def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+ end
+
+ def parse_json
+  JSON.parse(get_response_body)
+ end
+
+end
